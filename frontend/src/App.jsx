05879ccd89230bfe9bef5b6457e8fddaef8b1ee4
Sanah Rajesh <136401567+SanahR@users.noreply.github.com>
2026-02-25 18:40:46 -0600

    Add comment to describe app functionality
    Add a comment describing the app's functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+/* Simple front end with space for the input, the length slider, and the output */
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
